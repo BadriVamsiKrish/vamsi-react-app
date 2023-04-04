a6a7188493a11e3736cb58405dd8b454e5405e95
BadriVamsiKrish <1203badrivamsi@gmail.com>
2023-04-04 10:36:12 +0530

    added css to expenceitem

--- src/component/ExpenceItem.js
+++ src/component/ExpenceItem.js
@@ -1,11 +1,12 @@
+import './ExpenceItem.css';
 function ExpenceItem() {
   return(
-    <>
-    <h2>Expence Item</h2>
-    <h2>Food Rs 10</h2>
-    <h2>Petrol Rs 100</h2>
-    <h2>Movies Rs 200</h2>
-    </>
+    <div className="container">
+    <h2 className="heading">Expence Item</h2>
+    <h2 className="item">Food Rs 10</h2>
+    <h2 className="item">Petrol Rs 100</h2>
+    <h2 className="item">Movies Rs 200</h2>
+    </div>
     
   );
   
